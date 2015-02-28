6b594db1f88d6628e87a3b855748a0abea84d038
pcarceller <carcellerpablo@gmail.com>
2015-02-28 15:02:03 -0800

    Registration View AND rEGISTRATION cONTROLLER

---
+++ CloseTheGap/closethegap/Controllers/RegistrationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace closethegap.Controllers
+{
+
+        [RoutePrefix("user")]
+
+
+    public class RegistrationController : Controller
+    {
+
+        [Route("registration")]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+    }
+}
