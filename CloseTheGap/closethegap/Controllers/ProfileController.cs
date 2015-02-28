5be41a2cb97126a33ac617f0027312c939943372
CLAW <claudia.gon@gmail.com>
2015-02-28 14:26:42 -0800

    profile controller & profile create cshtml

---
+++ CloseTheGap/closethegap/Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace closethegap.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: UserProfiles
+       
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+ 
+}
